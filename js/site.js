9d86dad793f9229778ab251963810ad0c0e42baf
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-08-20 07:27:41 -0700

    08202021-Initial Commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,44 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello world");
+//Reads values from form
+function getValues(){
+    let fizzValue=parseInt(document.getElementById("fizzInput").value);
+    let buzzValue=parseInt(document.getElementById("buzzInput").value);
+    let fizzBuzzArray=fizzBuzz(fizzValue, buzzValue);
+    displayFizzBuzz(fizzBuzzArray);
+}
+//Builds FizzBuzz array
+function fizzBuzz(fizzValue, buzzValue){
+    values=[];
+    for(let i=0; i<100;i++){
+        if(i%fizzValue==0 && i%buzzValue==0){
+            values.push('FizzBuzz');
+        }
+        else if(i%fizzValue==0){
+            values.push('Fizz');
+        }
+        else if(i%buzzValue==0){
+            values.push('Buzz');
+        }
+        else{
+            values.push(i);
+        }
+
+    }
+    return values;
+}
+//Displays the fizzbuzz values in a table 
+function displayFizzBuzz(array){
+    let rowLength=5;
+    let rowTemplate='';
+    let tableTemplate='';
+    let cell=''
+    for(let i=0; i<array.length;i++){
+        for(let j=0; j<=rowLength;j++){
+            cell=`<td>${array[i]}<td>`
+            rowTemplate+=cell;
+        }
+        rowTemplate="<td>"+rowTemplate+"<td>";
+        tableTemplate+=rowTemplate;
+        rowTemplate='';
+    }
+    document.getElementById("fizzBuzzTable").innerHTML=tableTemplate;
 }
